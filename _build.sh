15f97580418ff4880e65794038507773d1a96701
Taavi Päll <tapa741@gmail.com>
2017-11-02 08:32:13 +0200

    fix some lines

--- _build.sh
+++ _build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-Rscript -e "fx <- inline::cxxfunction( signature(x = "integer", y = "numeric" ) , '
-	return ScalarReal( INTEGER(x)[0] * REAL(y)[0] ) ;
-' )
-fx( 2L, 5 ) # should be 10"
+Rscript -e 'fx <- inline::cxxfunction(signature(x = "integer", y = "numeric") ,
+"return ScalarReal(INTEGER(x)[0] * REAL(y)[0]);"); fx(2L, 5)'
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
