f2f5fe8f048a7b27fa4667df9406db0b7be7cdf3
Taavi Päll <tapa741@gmail.com>
2017-11-03 16:17:50 +0200

    messages

--- _deploy.sh
+++ _deploy.sh
@@ -3,14 +3,35 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
+
+echo "Pat ok!"
+
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
+echo "master branch ok!"
 
 git config --global user.email "tapa741@gmail.com"
 git config --global user.name "Taavi Päll"
 
+echo "credentials ok!"
+
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+
+echo "git clone ok!"
+
 cd book-output
 cp -r ../_book/* ./
+
+echo "cp _book ok!"
+
 git add --all *
+
+echo "git add ok!"
+
 git commit -m "Update the book" || true
+
+echo "commit ok!"
+
 git push -q origin gh-pages
+
+echo "push ok!"
