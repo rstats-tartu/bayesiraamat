be19ecab6b2c12640511bb0bd8cfbbb28e7cc961
Taavi Päll <tapa741@gmail.com>
2017-11-03 20:13:13 +0200

    try fix script

--- _build.sh
+++ _build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 echo "Testing rstan..."
-Rscript -e 'fx <- inline::cxxfunction(signature(x = "integer", y = "numeric") ,
-"return ScalarReal(INTEGER(x)[0] * REAL(y)[0]);")'
-Rscript -e 'fx(2L, 5)'
+Rscript -e "fx <- inline::cxxfunction(signature(x = 'integer', y = 'numeric') , 'return ScalarReal(INTEGER(x)[0] * REAL(y)[0]);')"
+Rscript -e "fx(2L, 5)"
 
 echo "Render book"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
