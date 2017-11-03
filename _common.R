ecda93100f1f2b5ada4f541cc6d74a84e3dd1790
Taavi Päll <tapa741@gmail.com>
2017-11-03 10:35:17 +0200

    theme tufte ja mitmeid teisi pisifixe

--- _common.R
+++ _common.R
@@ -1,3 +1,5 @@
+
+rm(list = ls(all = TRUE))
 set.seed(19)
 
 options(digits = 3,
@@ -8,11 +10,13 @@ knitr::opts_chunk$set(
   message = FALSE,
   comment = "#>",
   collapse = TRUE,
-  cache = TRUE,
   out.width = "70%",
   fig.align = 'center',
   fig.width = 6,
-  fig.asp = 0.618,  # 1 / phi
+  fig.asp = 0.7,  # 1 / phi
   fig.show = "hold"
 )
 
+library(ggplot2)
+library(ggthemes)
+old <- theme_set(theme_tufte())
