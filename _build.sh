55b9e7f8a8f3e07a33dfbb460508c5aac133defe
Taavi Päll <tapa741@gmail.com>
2017-11-03 18:48:38 +0000

    pdf?

--- _build.sh
+++ _build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-echo "Testing rstan..."
-Rscript -e "fx <- inline::cxxfunction(signature(x = 'integer', y = 'numeric') , 'return ScalarReal(INTEGER(x)[0] * REAL(y)[0]);')"
-Rscript -e "fx(2L, 5)"
-
 echo "Render book"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+
+Rscript -e "bookdown::render_book('index.Rmd')"
