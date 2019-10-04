ab16001037b1a5e42768625c64029f59297fbc75
Taavi Päll <tapa741@gmail.com>
2019-10-04 19:02:04 +0300

    updated commit message

--- _local_deploy.sh
+++ _local_deploy.sh
@@ -4,5 +4,5 @@ git clone -b gh-pages git@github.com:rstats-tartu/bayesiraamat.git book-output \
   && cd book-output \
   && cp -r ../_book/* ./ \
   && git add --all * \
-  && git commit -m "Update the book" \
+  && git commit -m "Update book" \
   && git push -q origin gh-pages
